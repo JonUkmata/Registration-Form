93789fa5f03a91615184fd60e62fbb72a8e8340c
Jon Ukmata <jon.ukmata@gmail.com>
2025-12-26 05:25:58 +0100

    fix

--- src/schemas/register-schema.ts
+++ src/schemas/register-schema.ts
@@ -4,7 +4,7 @@ export const registerSchema = z
   .object({
     fullName: z.string().min(2, "Full name is required"),
     email: z.string().email("Invalid email address"),
-    password: z.string().min(8, "Password must be at least 6 characters"),
+    password: z.string().min(8, "Password must be at least 8 characters"),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
